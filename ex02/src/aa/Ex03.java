0419fa6c47551e64838c4ba913dce8e728cb0025
DARAM1012 <xx021521@gmail.com>
2024-06-28 12:58:45 +0900

    0628

---
+++ ex02/src/aa/Ex03.java
@@ -0,0 +1,18 @@
+package aa;
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+
+		// byte - short - int cpu
+		short a = 11;
+		short b = 22;
+
+		// ctrl + 1 = 에러난 곳 잡아줌
+
+		short result = (short) (a + b);
+		System.out.println(a + b);
+
+	}
+
+}
---
+++ ex02/src/aa/Ex04.java
@@ -0,0 +1,20 @@
+package aa;
+
+// java springboot c#
+
+public class Ex04 {
+	
+	public static void main(String[] args) {
+		
+		String aa = "비비디바비디부";
+		char a = 'A';
+		char b = 'B';
+		
+		System.out.println((int)a);
+		System.out.println((int)b);
+		
+		System.out.println(aa);
+		
+	}
+
+}
