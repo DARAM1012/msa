2ec18bd7e76c2fdd58e531cfe20be567ec0c1610
DARAM1012 <xx021521@gmail.com>
2024-07-17 18:06:49 +0900

    0717

--- java_work/ex15/src/Ex08.java
+++ java_work/ex15/src/Ex08.java
@@ -12,7 +12,17 @@
 //매개변수 없음
 
 
+import java.util.Scanner;
+
 public class Ex08 {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        int n = sc.nextInt();
 
+        for (int i = 0;     i < n     ; i++) {
+            System.out.println(str);
+        }
+    }
 }
